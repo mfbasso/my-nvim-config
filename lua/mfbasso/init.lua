14db2fa15d5ca48a19e71afc1ca89aed0bc8ceb1
Marcos Basso <mfbasso@live.com>
2023-07-28 01:29:11 -0300

    Chore: Initial keymaps

--- lua/mfbasso/init.lua
+++ lua/mfbasso/init.lua
@@ -1,4 +1,5 @@
 require("mfbasso.remap")
+require("mfbasso.set")
 vim.cmd.colorscheme("night-owl")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
